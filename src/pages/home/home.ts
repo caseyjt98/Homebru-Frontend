985580e3c54c30a5800a04829b3dae781e9fa6fb
Casey Taylor <caseyjt98@gmail.com>
2018-06-26 11:41:53 +0200

    adding pages

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoginPage } from '../login/login'; 
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,15 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) {}
 
+  /** we want to link this function to our Log In button to make it do something */
+  navigateToLogin() {
+    /** use global object console to print "natigating" to your console, 
+     * good for debugging */
+    console.log("Navigating...") 
+
+    this.navCtrl.push(LoginPage); 
   }
 
 }
