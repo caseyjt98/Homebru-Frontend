8e36efd1013d7f7aa9d5c019be13478b30b9e669
Casey Taylor <caseyjt98@gmail.com>
2018-06-28 19:08:18 +0200

    pie chart added

--- src/pages/payment-history/payment-history.ts
+++ src/pages/payment-history/payment-history.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Chart } from 'chart.js';
 
 /**
  * Generated class for the PaymentHistoryPage page.
@@ -14,10 +15,39 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class PaymentHistoryPage {
 
+  @ViewChild('doughnutCanvas') doughnutCanvas;
+  doughnutChart: any;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
+
+    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+ 
+      type: 'doughnut',
+      data: {
+          labels: ["Water", "Electricity", "Parking", "Rent"],
+          datasets: [{
+              label: 'Breakdown of Monthly Expenses',
+              data: [130, 90, 50, 750],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)'
+    
+              ],
+              hoverBackgroundColor: [
+                  "#FF6384",
+                  "#36A2EB",
+                  "#FFCE56",
+                  "#FF6384"
+              ]
+          }]
+      }
+
+  });
     console.log('ionViewDidLoad PaymentHistoryPage');
   }
 
