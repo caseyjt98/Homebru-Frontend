ab2f92cb1326eef142f012eefc7b12d3b4ab1b11
Casey Taylor <caseyjt98@gmail.com>
2018-06-28 20:49:57 +0200

    set preferences

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { LoginPage } from '../login/login';
 import { Property } from '../../models/property';
 
 
