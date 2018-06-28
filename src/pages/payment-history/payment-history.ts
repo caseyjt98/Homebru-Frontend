740b5e7c3ef4c835fa45ea191166e1244184b504
Casey Taylor <caseyjt98@gmail.com>
2018-06-28 20:31:29 +0200

    add bar graph to payment history

--- src/pages/payment-history/payment-history.ts
+++ src/pages/payment-history/payment-history.ts
@@ -16,7 +16,9 @@ import { Chart } from 'chart.js';
 export class PaymentHistoryPage {
 
   @ViewChild('doughnutCanvas') doughnutCanvas;
+  @ViewChild('barCanvas') barCanvas;
   doughnutChart: any;
+  barChart: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
@@ -48,6 +50,59 @@ export class PaymentHistoryPage {
       }
 
   });
+
+  this.barChart = new Chart(this.barCanvas.nativeElement, {
+ 
+    type: 'bar',
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+        datasets: [{
+            label: 'Amount Payed',
+            data: [700, 800, 750, 900, 1000, 500, 600, 800, 650, 1000, 700, 900],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                'rgba(255,99,132,1)',
+                
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+
+});
+
     console.log('ionViewDidLoad PaymentHistoryPage');
   }
 
