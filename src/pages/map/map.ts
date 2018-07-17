ebcf9f8b991925a4d09f63b884bd8ba92fa51384
Casey Taylor <caseyjt98@gmail.com>
2018-07-17 11:14:14 +0200

    map works

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+declare var google;
 /**
  * Generated class for the MapPage page.
  *
@@ -19,6 +19,14 @@ export class MapPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MapPage');
+
+    // The location of Uluru
+    var uluru = {lat: -25.344, lng: 131.036};
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 4, center: uluru});
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({position: uluru, map: map});
   }
 
 }
