fd6bf639cfed6ce5b0d61057dd76fb4614c194fd
Casey Taylor <caseyjt98@gmail.com>
2018-07-17 14:17:42 +0200

    split up home, sign in pages

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,10 +2,9 @@
 
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { LoginPage } from '../login/login'; 
 import { RegistrationPage } from '../registration/registration';
-import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { SignInPage } from '../sign-in/sign-in';
 
 
 @Component({
@@ -16,71 +15,16 @@ import 'rxjs/add/operator/map';
 
 export class HomePage {
 
-  posts: any;
-
-  email: string;
-  password: string;
-
-  constructor(public navCtrl: NavController, private http: Http) {
+  constructor(public navCtrl: NavController) { }
 
 
-    if (localStorage.getItem("TOKEN")) {
-     // alert("Already logged in");
-    
-      this.http.get("http:localhost:3000/verify?jwt=" + localStorage.getItem("TOKEN")).subscribe(
-        result => {
-          console.log(result.json());
-        },
-  
-        err => {
-          // Invalid, login!
-        }
-      );
-
-    }
-    /**  
-    this.http.get('https://www.reddit.com/r/gifs/new/.json?limit=10').map(res => res.json()).subscribe(data => {
-        //this.posts = data.data.children;
-    });
-*/
+ navigateToSignIn() {
  
-  }
+  console.log("Navigating...") 
 
-  //link this function to our Log In button to make it do something 
-  navigateToLogin() {
- 
-    console.log("Navigating...") 
+  this.navCtrl.push(SignInPage); 
 
-    this.navCtrl.push(LoginPage); 
-  }
-
-  login() {
-    this.http
-      .post("http:localhost:3000/login", {
-        email: this.email,
-        password: this.password
-  })
-  .subscribe (
-    result => { 
-      console.log(result);
-
-      var jwtResponse = result.json();
-      var token = jwtResponse.token;
-
-      localStorage.setItem("TOKEN",token);
-
-      
-
-      this.navigateToLogin();
-    },
-    err => {
-      console.log(err);
-      alert("Failed to login");
-      this.navigateToLogin();
-    }
-  );
-
-  }
+ }
 
   navigateToRegistration() {
 
--- src/pages/sign-in/sign-in.ts
+++ src/pages/sign-in/sign-in.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { LoginPage } from '../login/login'; 
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 /**
  * Generated class for the SignInPage page.
@@ -14,7 +17,63 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class SignInPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  posts: any;
+
+  email: string;
+  password: string;
+
+  constructor(public navCtrl: NavController, private http: Http) {
+
+
+    if (localStorage.getItem("TOKEN")) {
+     // alert("Already logged in");
+    
+      this.http.get("http:localhost:3000/verify?jwt=" + localStorage.getItem("TOKEN")).subscribe(
+        result => {
+          console.log(result.json());
+        },
+  
+        err => {
+          // Invalid, login!
+        }
+      );
+
+    }
+  }
+
+  login() {
+    this.http
+      .post("http:localhost:3000/login", {
+        email: this.email,
+        password: this.password
+  })
+  .subscribe (
+    result => { 
+      console.log(result);
+
+      var jwtResponse = result.json();
+      var token = jwtResponse.token;
+
+      localStorage.setItem("TOKEN",token);
+
+      
+
+      this.navigateToLogin();
+    },
+    err => {
+      console.log(err);
+      alert("Failed to login");
+      this.navigateToLogin();
+    }
+  );
+
+  }
+
+  navigateToLogin() {
+ 
+    console.log("Navigating...") 
+
+    this.navCtrl.push(LoginPage); 
   }
 
   ionViewDidLoad() {
